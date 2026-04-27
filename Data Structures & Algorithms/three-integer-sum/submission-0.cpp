ee0d3bef1bbf26db258a1582d59187a2462c63ec
OrcaLover7 <vashistl1999@gmail.com>
2026-04-27 19:17:47 +0530

    Add: three-integer-sum - submission-0

---
+++ Data Structures & Algorithms/three-integer-sum/submission-0.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        
+        sort(nums.begin(), nums.end());
+
+        int n = nums.size();
+        vector<vector<int>> ans;
+
+        for(int i = 0; i < n; i++){
+            if(i > 0 && nums[i] == nums[i-1])
+                continue;
+            
+            int a = nums[i];
+
+            int target = -a;
+
+            int l = i+1, r = n-1;
+
+            while (l < r) {                                                                                                                                    
+                int sum = nums[l] + nums[r];                                                                                                                   
+                                                                                                                                                                
+                if (sum == target) {                                                                                                                           
+                    ans.push_back({a, nums[l], nums[r]});                                                                                                      
+                    while (l < r && nums[l] == nums[l+1]) l++;                                                                                                 
+                    while (l < r && nums[r] == nums[r-1]) r--;                                                                                                 
+                    l++;                                                                                                                                       
+                    r--;                                                                                                                                       
+                }                                                                                                                                              
+                else if (sum > target) {                                                                                                                       
+                    r--;                                                                                                                                       
+                }                                                                                                                                              
+                else {                                                                                                                                         
+                    l++;                                                                                                                                       
+                }                                                     
+            } 
+        }
+        
+        return ans;
+    }
+};
