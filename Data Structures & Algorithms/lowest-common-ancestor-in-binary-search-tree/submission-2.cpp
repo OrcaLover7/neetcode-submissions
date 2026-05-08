b31d299b41b7c5372d822f84d4fc091a8b3959a7
OrcaLover7 <vashistl1999@gmail.com>
2026-05-09 00:37:34 +0530

    Add: lowest-common-ancestor-in-binary-search-tree - submission-2

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-2.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    // one idea is to do bfs or dfs and save the paretns which would allow us to connect the root to node paths for p and q
+    // we could find the first common node on the path back from node to root
+    // but I wanna know how I can make use of the BST property
+    // if root->val lies between p and q, then root is teh answer
+    // else we check in one the subtrees
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        while (root) {                                                                                                                       
+            if (p->val < root->val && q->val < root->val) root = root->left;
+            else if (p->val > root->val && q->val > root->val) root = root->right;                                                           
+            else return root;                                                                                                                
+        }                                                                                                                                    
+        return nullptr;
+    }
+};
