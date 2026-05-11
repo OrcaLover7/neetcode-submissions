e77cf1411d34c002b2ec8e1d9f737f876df1426b
OrcaLover7 <vashistl1999@gmail.com>
2026-05-11 16:32:42 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        int l = 0, r = 0, profit = 0;
+
+        while(r < (int)prices.size()){
+            if(prices[r] < prices[l])
+                l = r;
+            profit = max(profit, prices[r]-prices[l]);
+            r++;
+        }
+
+        return profit;
+    }
+};
