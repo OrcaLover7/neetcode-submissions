a0e04b7d3316635afb3cfb131a0b0bd009868cc7
OrcaLover7 <vashistl1999@gmail.com>
2026-05-15 18:43:05 +0530

    Add: subsets - submission-3

---
+++ Data Structures & Algorithms/subsets/submission-3.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        
+        int n = (int)nums.size();
+        vector<vector<int>> subsets;
+
+        for(int i = 0; i < (1 << n); i++){
+            vector<int> v;
+            for(int j = 0; j < n; j++){
+                if(i & (1 << j)) v.push_back(nums[j]);
+            }
+            subsets.push_back(v);
+        }
+
+        return subsets;
+    }
+};
