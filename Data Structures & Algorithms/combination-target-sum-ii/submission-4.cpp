50cbd30de24821cbe68a6f2bb1807ef9b3ad856d
OrcaLover7 <vashistl1999@gmail.com>
2026-05-25 20:56:25 +0530

    Add: combination-target-sum-ii - submission-4

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-4.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        unordered_map<int, int> freq;
+        vector<int> uniq;
+
+        for(int n : candidates){
+            if(!freq[n]) uniq.push_back(n);
+            freq[n]++;
+        }
+        sort(uniq.begin(), uniq.end());
+
+        vector<int> combo;
+        vector<vector<int>> ans;
+
+        bt(uniq, freq, 0, target, combo, ans);
+
+        return ans;
+    }
+
+    void bt(vector<int>& uniq, unordered_map<int, int>& freq, int start, int target, vector<int>& combo, vector<vector<int>>& ans){
+        if(target == 0){
+            ans.push_back(combo);
+            return;
+        }
+
+        if(target < 0 || start >= (int)uniq.size()) return;
+
+        int curr = uniq[start];
+
+        if(curr > target) return;
+        
+        if(freq[curr]){
+            combo.push_back(curr);
+            freq[curr]--;
+            bt(uniq, freq, start, target - curr, combo, ans);
+            freq[curr]++;
+            combo.pop_back();
+        }
+
+        bt(uniq, freq, start+1, target, combo, ans);
+    }
+};
