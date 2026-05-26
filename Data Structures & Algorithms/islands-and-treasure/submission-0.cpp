8951c9449114e8c312dfa76b1a5d5fef1d3a8c81
OrcaLover7 <vashistl1999@gmail.com>
2026-05-26 22:05:35 +0530

    Add: islands-and-treasure - submission-0

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-0.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool isValid(int x, int y, int m, int n){
+        return (x >= 0 && y >= 0 && x < m && y < n);
+    }
+    void islandsAndTreasure(vector<vector<int>>& grid) {
+        queue<pair<int, int>> q;
+        int m = (int)grid.size();
+        int n = (int)grid[0].size();
+        int inf = 2147483647;
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == 0){
+                    q.push({i, j});
+                }
+            }
+        }
+
+        vector<pair<int, int>> directions = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+        
+        while(!q.empty()){
+            auto [x, y] = q.front();
+            q.pop();
+
+            for(auto [dx, dy] : directions){
+                int nx = x + dx;
+                int ny = y + dy;
+
+                if(isValid(nx, ny, m, n) && grid[nx][ny] == inf){
+                    grid[nx][ny] = grid[x][y] + 1;
+                    q.push({nx, ny});
+                }
+            }
+        }
+
+        return;
+    }
+};
