e7fa49f1ee6b32304792fa6a4d24eb0891e8a7ee
OrcaLover7 <vashistl1999@gmail.com>
2026-05-27 19:29:11 +0530

    Add: subsets-ii - submission-2

---
+++ Data Structures & Algorithms/subsets-ii/submission-2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        vector<int> sub;
+        vector<vector<int>> ans;
+
+        bt(nums, 0, sub, ans);
+
+        return ans;
+    }
+
+    void bt(vector<int>& nums, int start, vector<int>& sub, vector<vector<int>>& ans){
+        ans.push_back(sub);
+
+        for(int i = start; i < (int)nums.size(); i++){
+            if(i > start && nums[i] == nums[i-1]) continue;
+            sub.push_back(nums[i]);
+            bt(nums, i+1, sub, ans);
+            sub.pop_back();
+        }
+    }
+};
