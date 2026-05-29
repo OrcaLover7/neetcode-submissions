01fe10148e01be549fca0c86a5e0292dc749b40b
OrcaLover7 <vashistl1999@gmail.com>
2026-05-29 19:48:31 +0530

    Add: jump-game-ii - submission-3

---
+++ Data Structures & Algorithms/jump-game-ii/submission-3.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        
+        int n = (int)nums.size();
+        int farthest = 0;
+        int currend = 0;
+        int jump = 0;
+
+
+        for(int i = 0; i < n-1; i++){
+            farthest = max(farthest, i+ nums[i]);
+            if(i == currend){
+                currend = farthest;
+                jump++;
+            }
+        }
+
+        return jump;
+    }
+};
