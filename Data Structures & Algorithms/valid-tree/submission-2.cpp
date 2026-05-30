76c76001c89240de157440ed5e0598a43db77495
OrcaLover7 <vashistl1999@gmail.com>
2026-05-31 00:27:52 +0530

    Add: valid-tree - submission-2

---
+++ Data Structures & Algorithms/valid-tree/submission-2.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> parent, rank;
+
+    void make_set(int n){
+        parent.resize(n, -1);
+        rank.resize(n, 1);
+
+        for(int i = 0; i < n; i++) parent[i] = i;
+    }
+
+    int find_set(int a){
+        if(parent[a] == a) return a;
+        return parent[a] = find_set(parent[a]);
+    }
+
+    bool union_set(int a, int b){
+        a = find_set(a);
+        b = find_set(b);
+        if(a == b) return true;
+        if(rank[a] < rank[b]) swap(a, b);
+        rank[a] += rank[b];
+        parent[b] = a;
+        return false;
+    }
+    bool validTree(int n, vector<vector<int>>& edges) {
+        int edge_count = (int)edges.size();
+        if(edge_count != n-1) return false;
+
+        make_set(n);
+
+        for(auto& edge : edges) if(union_set(edge[0], edge[1])) return false;
+
+        return true;
+    }
+};
