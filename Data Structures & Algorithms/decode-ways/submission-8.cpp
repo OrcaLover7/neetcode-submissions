f95606535f266e0c465ad9761b02f468cf189dfd
OrcaLover7 <vashistl1999@gmail.com>
2026-05-30 12:40:40 +0530

    Add: decode-ways - submission-8

---
+++ Data Structures & Algorithms/decode-ways/submission-8.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        if(s[0] == '0') return 0;
+
+        int n = (int)s.size();
+        if(n == 1) return 1;
+
+        int a = 1, b = 1;
+
+        for(int i = 2; i <= n; i++){
+            bool b1 = (s[i-1] != '0') ? true : false;
+            int doub = (s[i-2] - '0')*10 + (s[i-1] - '0');
+            bool b2 = (doub >= 10 && doub <= 26) ? true : false;
+
+            int c = 0;
+
+            if(b1 && b2) c = a + b;
+            else if(b1 && !b2) c = b;
+            else if(!b1 && b2 ) c = a;
+            else c = 0;
+
+            a = b;
+            b = c;
+        }
+
+        return b;
+    }
+};
