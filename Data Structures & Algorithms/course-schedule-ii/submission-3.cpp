00ba57130a0cd41c15bc08b8eba91fbc702eab64
OrcaLover7 <vashistl1999@gmail.com>
2026-05-31 14:20:10 +0530

    Add: course-schedule-ii - submission-3

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-3.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& prereqs) {
+        vector<vector<int>> graph(n);
+        vector<int> indegree(n, 0);
+
+        for(auto& req : prereqs){
+            graph[req[1]].push_back(req[0]);
+            indegree[req[0]]++;
+        }
+
+        queue<int> q;
+
+        for(int i = 0; i < n; i++) if(!indegree[i]) q.push(i);
+
+        vector<int> ans;
+
+        while(!q.empty()){
+            int crs = q.front();
+            q.pop();
+            ans.push_back(crs);
+
+            for(auto& neigh : graph[crs]){
+                indegree[neigh]--;
+                if(!indegree[neigh]) q.push(neigh);
+            }
+        }
+
+        vector<int> empty;
+
+        return ((int)ans.size() == n) ? ans : empty;
+    }
+};
