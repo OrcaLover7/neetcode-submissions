f8aa0350a9855b7c653434daed11a8717350a35b
OrcaLover7 <vashistl1999@gmail.com>
2026-06-03 14:53:39 +0530

    Add: min-cost-to-connect-points - submission-5

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-5.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+
+    void make_set(int n, vector<int>& parent, vector<int>& sz){
+        parent.resize(n, -1);
+        sz.resize(n, 1);
+
+        for(int i = 0; i < n; i++) parent[i] = i;
+    }
+
+    int find_set(int x, vector<int>& parent){
+        return parent[x] = (parent[x] == x ? x : find_set(parent[x], parent));
+    }
+    
+    bool union_set(int x, int y, vector<int>& parent, vector<int>& sz){
+        x = find_set(x, parent);
+        y = find_set(y, parent);
+
+        if(x == y) return false;
+
+        if(sz[x] < sz[y]) swap(x, y);
+        sz[x] += sz[y];
+        parent[y] = x;
+
+        return true;
+    }
+
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        int n = (int)points.size();
+
+        vector<int> parent, sz;
+        make_set(n, parent, sz);
+
+        vector<array<int, 3>> edges;
+
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                int d = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]);
+                edges.push_back({d, i, j});
+            }
+        }
+
+        sort(edges.begin(), edges.end());
+        int cost = 0, count = 0;
+
+        for(auto& [d, i, j] : edges){
+            if(count == n-1) break;
+            if(union_set(i, j, parent, sz)){
+                cost += d;
+                count++;
+            }
+        }
+
+        return cost;
+
+    }
+};
