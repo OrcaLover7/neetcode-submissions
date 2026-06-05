8aad96383e9fc53037f4bb4af94222a817403ded
OrcaLover7 <vashistl1999@gmail.com>
2026-06-05 10:47:48 +0530

    Add: min-cost-to-connect-points - submission-6

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-6.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        int n = (int)points.size();
+        vector<long long> dist(n, LLONG_MAX);
+        vector<bool> in(n, false);
+
+        long long cost = 0;
+
+        dist[0] = 0;
+
+        for(int i = 0; i < n; i++){
+            int u = -1;
+
+            for(int v = 0; v < n; v++) if(!in[v] && (u == -1 || dist[v] < dist[u])) u = v;
+
+            in[u] = true;
+            cost += dist[u];
+
+            for(int w = 0; w < n; w++){
+                if(in[w]) continue;
+
+                long long d = abs(points[w][0] - points[u][0]) + abs(points[w][1] - points[u][1]);
+                dist[w] = min(dist[w], d);
+            }
+        }
+
+        return (int)cost;
+    }
+};
