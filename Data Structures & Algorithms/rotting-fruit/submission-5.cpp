7a5edca6f5c2d2d9b9fcb27b33630792038c7a9e
OrcaLover7 <vashistl1999@gmail.com>
2026-06-08 10:35:24 +0530

    Add: rotting-fruit - submission-5

---
+++ Data Structures & Algorithms/rotting-fruit/submission-5.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+
+    bool isValid(int x, int y, int m, int n){
+        return (x >= 0 && y >= 0 && x < m && y < n);
+    }
+    int orangesRotting(vector<vector<int>>& grid) {
+        int m = (int)grid.size();
+        int n = m ? (int)grid[0].size() : 0; //unnecessary but just for general problems, making it a habit
+
+        int fresh = 0;
+        queue<pair<int, int>> q;
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == 2) q.push({i, j});
+                if(grid[i][j] == 1) fresh++;
+            }
+        }
+
+        vector<pair<int, int>> directions = { {0, 1}, {0, -1}, {1, 0}, {-1, 0} };
+
+        int time = 0;
+
+        while(!q.empty()){
+            int sz = (int)q.size();
+
+            for(int i = 0; i < sz; i++){
+                auto [x, y] = q.front();
+                q.pop();
+
+                for(auto& [dx, dy] : directions){
+                    int nx = x + dx;
+                    int ny = y + dy;
+
+                    if(isValid(nx, ny, m, n) && grid[nx][ny] == 1){
+                        grid[nx][ny] = 2;
+                        fresh--;
+                        q.push({nx, ny});
+                    }
+                }
+            }
+
+            if(!q.empty()) time++;
+        }
+
+        return fresh == 0 ? time : -1;
+    }
+};
