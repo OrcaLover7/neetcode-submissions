ddfc2c47cb8cbc7cda08aff16e232511aae96dcb
OrcaLover7 <vashistl1999@gmail.com>
2026-07-05 13:07:13 +0530

    Add: longest-increasing-subsequence - submission-9

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-9.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = (int)nums.size();
+        vector<int> tails;
+
+        tails.push_back(nums[0]);
+
+        for(int i = 1; i < n; i++){
+            if(nums[i] > tails.back()) tails.push_back(nums[i]);
+
+            int idx = lower_bound(tails.begin(), tails.end(), nums[i]) - tails.begin();
+            tails[idx] = nums[i];
+        }
+
+        return (int)tails.size();
+    }
+};
