910f836591d95c38849306831bcf990d41c44685
OrcaLover7 <vashistl1999@gmail.com>
2026-07-18 22:41:27 +0530

    Add: buy-and-sell-crypto-with-cooldown - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = (int)prices.size();
+
+        vector<vector<int>> dp(n, vector<int> (3, 0));
+        //j = 0 -> hold on day i
+        //j = 1 -> sell on day i
+        //j = 2 -> rest on day i
+
+        dp[0][0] = -prices[0];
+        dp[0][1] = 0;
+        dp[0][2] = 0;
+
+        for(int i = 1; i < n; i++){
+            dp[i][0] = max(dp[i-1][0], dp[i-1][2] - prices[i]);
+            dp[i][1] = dp[i-1][0] + prices[i];
+            dp[i][2] = max(dp[i-1][2], dp[i-1][1]);
+        }
+
+        return max(dp[n-1][1], dp[n-1][2]);
+    }
+};
