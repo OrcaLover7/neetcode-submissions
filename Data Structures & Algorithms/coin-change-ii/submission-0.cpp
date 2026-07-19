6f8607c99dc65ccdad980e6ef1a13008c82b32f7
OrcaLover7 <vashistl1999@gmail.com>
2026-07-19 18:16:00 +0530

    Add: coin-change-ii - submission-0

---
+++ Data Structures & Algorithms/coin-change-ii/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount+1, 0);
+
+        dp[0] = 1;
+        sort(coins.begin(), coins.end());
+
+        for(int coin : coins){
+            for(int i = 1; i <= amount; i++){
+                int prev = i - coin;
+                if(prev >= 0) dp[i] += dp[prev];
+            }
+        }
+
+        return dp[amount];
+
+    }
+};
