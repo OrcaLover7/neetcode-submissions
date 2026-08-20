10813104a064a50a78a0e9e23d71d295078ef44b
OrcaLover7 <vashistl1999@gmail.com>
2026-08-20 08:09:51 +0530

    Add: trapping-rain-water - submission-6

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-6.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = (int)height.size();
+        int ml = height[0], mr = height[n-1];
+        int l = 0, r = n-1, area = 0;
+
+        while(l < r){
+            if(ml < mr){
+                l++;
+                ml = max(height[l], ml);
+                area += ml - height[l];
+            }
+            else{
+                r--;
+                mr = max(height[r], mr);
+                area += mr - height[r];
+            }
+        }
+
+        return area;
+    }
+};
